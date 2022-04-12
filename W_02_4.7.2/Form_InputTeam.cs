bfd5e532bf28c8851a9d4ca4f93a100fdb04959f
Lulhoangminh <hoangminhbadao@gmail.com>
2022-04-12 17:52:25 +0700

    Syncing

--- W_02_4.7.2/Form_InputTeam.cs
+++ W_02_4.7.2/Form_InputTeam.cs
@@ -80,19 +80,35 @@ namespace W_02_4._7._2
         {
             this.Close();
         }
-
+        public static string team1_name = "";
+        public static string team2_name = "";
         private void comboBox_player1_SelectionChangeCommitted(object sender, EventArgs e)
         {
             if (comboBox_player1.SelectedIndex != null)
             {
-                string selected = comboBox_player1.SelectedValue.ToString();
-
+                string selected_team_id = comboBox_player1.SelectedValue.ToString();
+                string selected_team_name = comboBox_player1.GetItemText(comboBox_player1.SelectedItem);
+                team1_name = selected_team_id;
             }
         }
 
         private void comboBox_player2_SelectionChangeCommitted(object sender, EventArgs e)
         {
+            if (comboBox_player2.SelectedIndex != null)
+            {
+                string selected_team_id = comboBox_player2.SelectedValue.ToString();
+                string selected_team_name = comboBox_player2.GetItemText(comboBox_player2.SelectedItem);
+                team2_name = selected_team_id;
+            }
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(team1_name == team2_name || team1_name == "" || team2_name == "")
+            {
+                string Error_dup = "Thông tin 2 người chơi không được phép trùng nhau hoặc bỏ trống";
+                MessageBox.Show(Error_dup, "Warning!", MessageBoxButtons.OK);
+            }
         }
     }
 }
